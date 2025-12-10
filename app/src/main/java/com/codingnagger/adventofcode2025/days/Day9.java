2bcefba2957438829b8d49f7f3c0ad1cd6f8979e
CodingNagger <contact@codingnagger.com>
2025-12-10 00:38:27 +0000

    Day 9 - part 2 almost there

--- app/src/main/java/com/codingnagger/adventofcode2025/days/Day9.java
+++ app/src/main/java/com/codingnagger/adventofcode2025/days/Day9.java
@@ -1,8 +1,6 @@
 package com.codingnagger.adventofcode2025.days;
 
 import java.util.*;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class Day9 implements Day {
     @Override
@@ -24,143 +22,61 @@ public class Day9 implements Day {
     @Override
     public String partTwo(List<String> input) {
         final var redTiles = input.stream().map(Position::parse).toList();
-        final var greenTiles = findGreenTiles(redTiles);
-        final var greenOrRedTiles = Stream.concat(redTiles.stream(), greenTiles.stream()).collect(Collectors.toUnmodifiableSet());
 
-        final var rectangles = new PriorityQueue<Rectangle>(Comparator.comparingDouble(r -> -r.area()));
+        final var rectangles = new ArrayList<Rectangle>();
 
         for (var potentialCornerA : redTiles) {
             for (var potentialCornerB : redTiles) {
                 if (potentialCornerA == potentialCornerB) continue;
 
-                final var rectangle = Rectangle.create(potentialCornerA, potentialCornerB);
-
-                if (rectangle.isFilledWith(greenOrRedTiles)) {
-                    rectangles.add(Rectangle.create(potentialCornerA, potentialCornerB));
-                }
+                rectangles.add(Rectangle.create(potentialCornerA, potentialCornerB));
             }
         }
 
-        return "" + rectangles.poll().area();
+        final var expectedRedTileCountInRectangle = 2;
+        final var flatRectangles = rectangles.stream()
+                .filter(Rectangle::isFlat)
+                .filter(r -> redTiles.stream().filter(r::contains).count() == expectedRedTileCountInRectangle)
+                .toList();
+
+        final var connectingGreenOrRedTiles = flatRectangles.stream()
+                .flatMap(r -> r.allPositions().stream())
+                .toList();
+
+        // still need to fill connectingTileShape with other greens otherwise the 4 corners check fails
+        final var filteredRectangles = rectangles.stream()
+                .filter(r -> redTiles.stream().filter(r::containsCorner).count() == expectedRedTileCountInRectangle)
+                .filter(r -> connectingGreenOrRedTiles.stream().filter(r::containsCorner).count() == 4)
+                .toList();
+
+        final var sortedRectangles = new PriorityQueue<Rectangle>(Comparator.comparingDouble(r -> -r.area()));
+        sortedRectangles.addAll(filteredRectangles);
+
+        return "" + sortedRectangles.poll().area();
     }
 
-    private List<Position> findGreenTiles(List<Position> redTiles) {
-        final var connectingGreenTiles = findConnectingGreenTiles(redTiles);
-        final var innerGreenTiles = findInnerGreenTiles(redTiles, connectingGreenTiles);
-
-        final var result = new ArrayList<Position>();
-
-        result.addAll(connectingGreenTiles);
-        result.addAll(innerGreenTiles);
-
-        return result;
-    }
-
-    private List<Position> findConnectingGreenTiles(List<Position> redTiles) {
-        var cursor = redTiles.getFirst();
-        var minX = cursor.x();
-        var maxX = cursor.x();
-        var minY = cursor.y();
-        var maxY = cursor.y();
-
-        for (var position : redTiles) {
-            minX = Math.min(minX, position.x());
-            maxX = Math.max(maxX, position.x());
-            minY = Math.min(minY, position.y());
-            maxY = Math.max(maxY, position.y());
-
-            if (minX == position.x()) {
-                cursor = position;
-            }
-        }
-
-        final var queryableRedTiles = new HashSet<>(redTiles);
-        final var greenTiles = new ArrayList<Position>();
-        final var visitedRedTiles = new HashSet<Position>();
-
-        exploration:
-        while (visitedRedTiles.size() < redTiles.size()) {
-            visitedRedTiles.add(cursor);
-
-            for (var y = cursor.y() - 1; y >= minY; y--) {
-                final var candidate = new Position(cursor.x(), y);
-
-                if (candidate.equals(cursor)) continue;
-
-                if (queryableRedTiles.contains(candidate)) {
-                    for (var yy = candidate.y + 1; yy < cursor.y(); ++yy) {
-                        greenTiles.add(new Position(cursor.x(), yy));
-                    }
-
-                    cursor = candidate;
-                    continue exploration;
-                }
-            }
-
-            for (var y = cursor.y() + 1; y <= minY; y++) {
-                final var candidate = new Position(cursor.x(), y);
-
-                if (candidate.equals(cursor)) continue;
-
-                if (queryableRedTiles.contains(candidate)) {
-                    for (var yy = candidate.y - 1; yy > cursor.y(); --yy) {
-                        greenTiles.add(new Position(cursor.x(), yy));
-                    }
-
-                    cursor = candidate;
-                    continue exploration;
-                }
-            }
-
-            for (var x = cursor.x() - 1; x >= minX; x--) {
-                final var candidate = new Position(x, cursor.y());
-
-                if (candidate.equals(cursor)) continue;
-
-                if (queryableRedTiles.contains(candidate)) {
-                    for (var xx = candidate.x + 1; xx < cursor.x(); ++xx) {
-                        greenTiles.add(new Position(xx, cursor.y()));
-                    }
-
-                    cursor = candidate;
-                    continue exploration;
-                }
-            }
-
-            for (var x = cursor.x() + 1; x <= maxX; x++) {
-                final var candidate = new Position(x, cursor.y());
-
-                if (candidate.equals(cursor)) continue;
-
-                if (queryableRedTiles.contains(candidate)) {
-                    for (var xx = candidate.x - 1; xx > cursor.x(); --xx) {
-                        greenTiles.add(new Position(xx, cursor.y()));
-                    }
-
-                    cursor = candidate;
-                    continue exploration;
-                }
-            }
-        }
-
-        return greenTiles;
-    }
-
-
-    private List<Position> findInnerGreenTiles(List<Position> redTiles, List<Position> connectingGreenTiles) {
+    private List<Position> findInnerGreenTiles(List<Position> connectingGreenOrRedTiles) {
         return null;
     }
 
 
     private record Rectangle(Position topLeft, Position bottomRight) {
         public long area() {
-            final var topRight = new Position(bottomRight.x(), topLeft.y());
-            final var bottomLeft = new Position(topLeft.x(), bottomRight.y());
+            final var topRight = getTopRight();
+            final var bottomLeft = getBottomLeft();
 
             final var sideA = bottomRight.distanceTo(topRight);
             final var sideB = bottomRight.distanceTo(bottomLeft);
 
             return sideA * sideB;
+        }
+
+        private Position getBottomLeft() {
+            return new Position(topLeft.x(), bottomRight.y());
+        }
+
+        private Position getTopRight() {
+            return new Position(bottomRight.x(), topLeft.y());
         }
 
         static Rectangle create(Position a, Position b) {
@@ -201,14 +117,32 @@ public class Day9 implements Day {
             return result;
         }
 
-        public boolean isFilledWith(Set<Position> greenOrRedTiles) {
-            final var containedTilesCount = greenOrRedTiles.stream().filter(this::contains).count();
-            return area() == containedTilesCount;
-        }
-
         private boolean contains(Position position) {
             return topLeft.x() <= position.x() && topLeft.y() <= position.y()
                     && bottomRight.x() >= position.x() && bottomRight.y() >= position.y();
+        }
+
+        public boolean isFlat() {
+            return topLeft.x() == bottomRight().x() || topLeft.y() == bottomRight().y();
+        }
+
+        public boolean containsCorner(Position position) {
+            return topLeft().equals(position)
+                    || bottomRight().equals(position)
+                    || getTopRight().equals(position)
+                    || getBottomLeft().equals(position);
+        }
+
+        public List<Position> allPositions() {
+            final var positions = new ArrayList<Position>();
+
+            for (var x = topLeft.x(); x <= bottomRight().x(); x++) {
+                for (var y = topLeft.y(); y <= bottomRight().y(); y++) {
+                    positions.add(new Position(x, y));
+                }
+            }
+
+            return positions;
         }
     }
 
